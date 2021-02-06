9f6bb04ef236029852107c9c78aae6a78da08f76
charlottexiao123 <63084824+charlotte-xiao@users.noreply.github.com>
2021-02-06 15:10:44 +0800

    优化代码
    优化代码

--- DailyHealth.py
+++ DailyHealth.py
@@ -7,18 +7,12 @@ def autofile(ID,passnumber):
     try:
         chromedriver=r'.\chromedriver.exe'
         browser=webdriver.Chrome(chromedriver)
-        url1 = "https://authserver.nuist.edu.cn/authserver/login?service=http://my.nuist.edu.cn/index.portal"
-        url2 = "http://e-office2.nuist.edu.cn/taskcenter/workflow/index"
-        url3 = "http://e-office2.nuist.edu.cn/infoplus/form/XNYQSB/start"
+        url = "http://e-office2.nuist.edu.cn/infoplus/form/XNYQSB/start"
         browser.implicitly_wait(10)
         browser.get(url1)
         browser.find_element_by_css_selector("#username").send_keys(ID)
         browser.find_element_by_css_selector("#password").send_keys(passnumber)
         browser.find_element_by_css_selector("#login_submit").click()
-        time.sleep(5)
-        browser.get(url2)
-        time.sleep(5)
-        browser.get(url3)
         browser.find_element_by_css_selector("input[name='fieldCNS']").click()
         browser.find_element_by_css_selector("input[name='fieldSTQKfrtw']").send_keys("36.8")
         browser.execute_script("$('nobr:contains(确认填报)').click()")
@@ -44,4 +38,5 @@ def notify(text):
 
 
 #填写账号和密码
-autofile("账号","密码")
+if __name__ == "__main__":
+    autofile("账号","密码")
